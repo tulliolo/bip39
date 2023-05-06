f05fdcda29de4e0d1f0851e2747caaf21c81b718
tulliolo <tulliolo@yahoo.com>
2023-05-06 18:48:56 +0200

    single load function

--- tests/common.py
+++ tests/common.py
@@ -18,27 +18,15 @@
 #   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 #   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 import json
-import secrets
-import string
 from pathlib import Path
 
-import pytest
 
-
-def __load_data():
+def load_data(section: str = "") -> dict:
     path = Path(__file__).parent
 
     with open(f"{path}/data/test_vectors.json") as file:
-        return json.load(file)
+        data = json.load(file)
 
-
-def get_vector_data():
-    return [
-        vector for vector in __load_data()["vector"]
-    ]
-
-
-def get_error_data(key: str):
-    return [
-        error for error in __load_data()["error"] if key in error
-    ]
+    if section:
+        data = data[section]
+    return data
